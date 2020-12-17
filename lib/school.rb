b84a04917271614447a1921ae96c857b6c30dd48
Romano Livi <romano_livi@outlook.com>
2020-12-17 01:03:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,18 +1,18 @@
 class School 
   
-  def roster 
-    @roster
-  end
-  
   def initialize(name)
     @name = name
     @roster = {}
   end
   
+  def roster 
+    @roster
+  end
+  
   def add_student(name, grade)
-    hash["student_roster"] = []
-    hash["student_roster"] = grade
-    hash["student_roster"][grade] << name
+    @name = name
+    @grade = grade
+    
   end
   
 end
