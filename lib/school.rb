a9d0992d946feffeeb26e8a7a21b4b2a6a950922
Romano Livi <romano_livi@outlook.com>
2020-12-17 00:42:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,7 @@
 class School 
   
   def roster 
-    roster = {}
+      
   end
   
   def initialize(name)
@@ -9,6 +9,7 @@ class School
   end
   
   def add_student(name, grade)
+    
   end
   
 end
